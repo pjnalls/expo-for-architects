802ed2623ab224fa7b7140e9958c03f16ae3eb89
pjnalls <42269808+pjnalls@users.noreply.github.com>
2025-04-02 11:46:19 -0500

    fix(tabs): define button to catfact screen

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -40,6 +40,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="catfact"
+        options={{
+          title: 'Cat Fact',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="cat.fill" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
