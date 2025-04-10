d7849d181d8330d07881889d90ae7f2bdcf71809
pjnalls <42269808+pjnalls@users.noreply.github.com>
2025-04-09 19:50:21 -0500

    feat(catfact): implement checkboxes for different breeds

--- app/(tabs)/catfact.tsx
+++ app/(tabs)/catfact.tsx
@@ -1,4 +1,5 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, TouchableOpacity, View, Text } from 'react-native';
+import { useEffect, useState } from 'react';
 // import { useLocalSearchParams } from 'expo-router';
 
 import ParallaxScrollView from '@/components/ParallaxScrollView';
@@ -6,9 +7,53 @@ import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { IconSymbol } from '@/components/ui/IconSymbol';
 import { useCatFact } from '@/contexts/CatFactContext';
+import { useTheme } from '@react-navigation/native';
+
+type CatBreed = {
+  id?: string;
+  name: string;
+  checked: boolean;
+};
+
+function CatBreedItem({ name, checked }: CatBreed) {
+  const { dark } = useTheme();
+  return checked ? (
+    <View className={`${dark ? 'bg-[#6EA]' : 'bg-[#174]'} rounded-md p-2`}>
+      <Text className={`${!dark ? 'text-[#7FB]' : 'text-[#142]'} text-lg`}>
+        ✓ {name}
+      </Text>
+    </View>
+  ) : (
+    <ThemedView className='rounded-md p-2'>
+      <ThemedText>❏ {name}</ThemedText>
+    </ThemedView>
+  );
+}
+
 export default function CatFactScreen() {
   // const item = useLocalSearchParams();
   const { catFact } = useCatFact();
+  const [catBreeds, setCatBreeds] = useState<CatBreed[] | undefined>(undefined);
+
+  const handleCatBreedChecked = (id?: string) => {
+    if (!id) return;
+    setCatBreeds(
+      catBreeds?.map((breed) =>
+        breed.id === id ? { ...breed, checked: !breed.checked } : breed
+      )
+    );
+  };
+
+  useEffect(() => {
+    setCatBreeds([
+      { id: '1', name: 'Persian', checked: false },
+      { id: '2', name: 'Siamese', checked: false },
+      { id: '3', name: 'Maine Coon', checked: false },
+      { id: '4', name: 'Ragdoll', checked: false },
+      { id: '5', name: 'Sphynx', checked: false },
+    ]);
+  }, []);
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
@@ -19,11 +64,25 @@ export default function CatFactScreen() {
           name="cat"
           style={styles.headerImage}
         />
-      }>
+      }
+    >
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Cat Fact</ThemedText>
       </ThemedView>
       <ThemedText>{catFact?.fact}</ThemedText>
+      {catBreeds?.map(({ name, checked, id }) => (
+        <TouchableOpacity
+          key={`CatBreedItem-${name}-${id}`}
+          onPress={() => {
+            handleCatBreedChecked(id);
+          }}
+        >
+          <CatBreedItem
+            name={name}
+            checked={checked}
+          />
+        </TouchableOpacity>
+      ))}
     </ParallaxScrollView>
   );
 }
