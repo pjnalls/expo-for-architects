5ad4e4e6fc8c040f766e41917778342c6d2e62e0
pjnalls <42269808+pjnalls@users.noreply.github.com>
2025-05-25 15:07:37 -0500

    feat(ai): integrate nlp into the ui

--- app/(tabs)/catfact.tsx
+++ app/(tabs)/catfact.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, TouchableOpacity } from 'react-native';
 // import { useLocalSearchParams } from 'expo-router';
 
 import ParallaxScrollView from '@/components/ParallaxScrollView';
@@ -6,12 +6,44 @@ import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { IconSymbol } from '@/components/ui/IconSymbol';
 import { useCatFact } from '@/contexts/CatFactContext';
-
-
+import ThemedTextInput from '@/components/ThemedTextInput';
+import { useState } from 'react';
+import { getClassification } from '@/api/endpoints/Nlp';
+import { Cat } from '@/types/Cat';
+import ThemedButton from '@/components/ThemedButton';
 
 export default function CatFactScreen() {
   // const item = useLocalSearchParams();
   const { catFact } = useCatFact();
+  const [name, setName] = useState('');
+  const [origin, setOrigin] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleNameChange = (text: string) => {
+    setName(text);
+  };
+
+  const handleClassifyName = async () => {
+    setOrigin('');
+
+    const cat: Cat = {
+      firstName: name,
+      lastName: 'Leon',
+      email: 'leon@gmail.com',
+      birthDate: new Date().toISOString(),
+      catBreed: [
+        {
+          name: 'Persian',
+          checked: true,
+        },
+      ],
+    };
+
+    setIsLoading(true);
+    const origin = await getClassification(cat);
+    setOrigin(origin ?? 'Unknown');
+    setIsLoading(false);
+  };
 
   return (
     <ParallaxScrollView
@@ -28,7 +60,39 @@ export default function CatFactScreen() {
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Cat Fact</ThemedText>
       </ThemedView>
-      <ThemedText>{catFact?.fact}</ThemedText>
+      <ThemedText>{`${catFact?.fact.substring(0, 100)}...`}</ThemedText>
+      <ThemedView>
+        <ThemedText>Hey AI, where does this cat</ThemedText>
+        <ThemedTextInput
+          onChangeText={handleNameChange}
+          label="name"
+          value={name}
+          isValid={/[A-Za-z]+/.test(name)}
+          errorMessage="Please enter a valid name"
+        />
+        <ThemedText>come from?</ThemedText>
+        <ThemedButton
+          onPress={handleClassifyName}
+          disabled={isLoading || !name || name.length < 3}
+          title="Classify Name"
+        ></ThemedButton>
+        {!isLoading && origin ? (
+          <ThemedView style={{ height: 200, marginTop: 20 }}>
+            <ThemedText type="title" style={{ textAlign: 'center' }}>
+              {`🤖:\n`}
+            </ThemedText>
+            <ThemedText style={{ textAlign: 'center' }}>{origin}</ThemedText>
+          </ThemedView>
+        ) : isLoading && !origin ? (
+          <ThemedView style={{ height: 200, marginTop: 20 }}>
+            <ThemedText>{'AI Classifying Name...'}</ThemedText>
+          </ThemedView>
+        ) : (
+          <ThemedView style={{ height: 200, marginTop: 20 }}>
+            <ThemedText>{'Please enter a name and click the button.'}</ThemedText>
+          </ThemedView>
+        )}
+      </ThemedView>
     </ParallaxScrollView>
   );
 }
